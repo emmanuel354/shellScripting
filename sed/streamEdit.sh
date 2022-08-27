d9b757e85d77592403165d8c34cc378ea1df4bdf
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-08-27 15:09:13 +0300

    update

--- sed/streamEdit.sh
+++ sed/streamEdit.sh
@@ -2,3 +2,7 @@
 
 #changes the value occurence of red in fruit.txt and redirect it to a new file
 sed -i 's/red/yellow/' fruits.txt  
+# '/' used is just a delimeter it really doesn't matter but its commonly used
+
+echo "hello" | sed 's/hello/changed value/'
+
